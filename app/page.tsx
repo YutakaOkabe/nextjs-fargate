71e94e0748ad82fa2168cd919b0bebb3a949e99e
YutakaOkabe <okabe.yutaka.833@gmail.com>
2025-03-01 20:08:16 +0900

    hello world

--- app/page.tsx
+++ app/page.tsx
@@ -8,31 +8,16 @@ type User = {
 export default async function UsersPage() {
   let users: User[] = [];
 
-  try {
-    const result = await query('SELECT * FROM users');
-    users = result.rows;
-  } catch (error) {
-    console.error('Error fetching users:', error);
-  }
+  // try {
+  //   const result = await query('SELECT * FROM users');
+  //   users = result.rows;
+  // } catch (error) {
+  //   console.error('Error fetching users:', error);
+  // }
 
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <p>Hello World</p>
     </div>
   );
 }
